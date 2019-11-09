b350d433a5c88ebbaa1e1eb4921f1e07fe5b1050
Mahesh Sompalli <msompalli@webtogo.de>
2019-11-09 12:28:25 +0530

    inital commit

---
+++ app/src/main/java/com/manu/gitlab/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.manu.gitlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manu.gitlab.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
